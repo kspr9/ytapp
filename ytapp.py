155715688df55aa69ddf156babfffc0c4dae3c18
kspr9 <kaspar.pae@gmail.com>
2022-06-24 15:12:35 +0300

    Basic logic done, included Colorama for styling

--- ytapp.py
+++ ytapp.py
@@ -1,25 +1,48 @@
 from pytube import YouTube
-from ytdl import dl_video, get_video_info
+import pytube.request
+from colorama import init, Fore, Back, Style
 
-link = str(input("Enter Youtube URL: "))
+# Change the value here to something smaller to decrease chunk sizes,
+#  thus increasing the number of times that the progress callback occurs
+pytube.request.default_range_size = 437184  # 9MB chunk size
 
-# info about video
-get_video_info(link)
+init()
+#link = str(input("Enter Youtube URL: "))
+link = "https://www.youtube.com/watch?v=CML6vfKjQss"
 
-# Downloading
-dl_video(link)
+def on_complete(stream, filepath):
+    print("Download complete!")        
+    print(filepath)
+        
+def on_progress(stream, chunk, bytes_remaining):
+    progress_string = f'{round(100 - (bytes_remaining / stream.filesize * 100),1)}%'
+    print("Downloading...")
+    print(progress_string)
 
-# so here is basically the same logic that I had under the main function of the ytdl.py program
+video_object = YouTube(link, on_complete_callback = on_complete, on_progress_callback = on_progress)
 
-'''
-print('Download: (b)est | (w)orst | (a)udio | (e)xit')
+def get_video_info(video_object):
+    print('\n<------------------------  Video info  --------------------------------->\n')
+    print(Fore.GREEN + Back.CYAN + Style.BRIGHT + f' Title:  {Style.RESET_ALL}{Style.BRIGHT} {video_object.title}')
+    print(Fore.GREEN + Back.CYAN + Style.BRIGHT + f' Length: {Style.RESET_ALL}{Style.BRIGHT} {round(video_object.length / 60,2)} minutes')
+    print(Fore.GREEN + Back.CYAN + Style.BRIGHT + f' Views:  {Style.RESET_ALL}{Style.BRIGHT} {round(video_object.views / 1000000,3)} million')
+    print(Fore.GREEN + Back.CYAN + Style.BRIGHT + f' Channel:{Style.RESET_ALL}{Style.BRIGHT} {video_object.author}')
+    print(Fore.GREEN + f'    URL:  {Style.RESET_ALL}{Style.BRIGHT}{link}')
+    print('\n<---------------------------------------------------------------------->\n')
+
+get_video_info(video_object)
+
+print(
+    Fore.BLUE + Style.BRIGHT + 'Download: ' + 
+    Fore.GREEN + Style.BRIGHT + f'(b)est {Style.RESET_ALL}|' + 
+    Fore.YELLOW + Style.BRIGHT + f'(w)orst {Style.RESET_ALL}|' +
+    Fore.CYAN + Style.BRIGHT + f'(a)udio {Style.RESET_ALL}{Style.BRIGHT}| (e)xit'
+    )
 download_choice = input('Choice: ')
 
-match download_choice:
-    case 'b':
-        video_object.streams.get_highest_resolution().download(r'/home/kaspar/Downloads')
-    case 'w':
-        video_object.streams.get_worst_resolution().download(r'/home/kaspar/Downloads')
-    case 'a':
-        video_object.streams.get_audio_only().download(r'/home/kaspar/Downloads')
-'''
+if download_choice == "b": 
+    video_object.streams.get_highest_resolution().download()
+if download_choice == 'w':
+    video_object.streams.get_lowest_resolution().download()
+if download_choice == 'a': 
+    video_object.streams.get_audio_only().download()
--- ytdl.py
+++ ytdl.py
@@ -1,15 +1,9 @@
 from pytube import YouTube
 
 
-def get_video_info(url):
-    video_object = YouTube(url)
-    print(f'title:  {video_object.title}')
-    print(f'length: {round(video_object.length / 60,2)} minutes')
-    print(f'views:  {round(video_object.views / 1000000,3)} million')
-    print(f'author: {video_object.author}')
 
-def get_video_object(url, stream):
-        
+def get_video_object(url):
+    url = url
     def on_complete(stream, filepath):
         print("Download complete!")
         print(filepath)
@@ -18,29 +12,35 @@ def get_video_object(url, stream):
         progress_string = f'100 - {round(bytes_remaining / stream.filesize * 100,2)}%'
         print(progress_string)
     
-    return(video_object = YouTube(url, on_complete_callback = on_complete, on_progress_callback = on_progress))
+    video_object = YouTube(url, on_complete_callback = on_complete, on_progress_callback = on_progress)
 
-def dl_video(url):
-    
-    print("\n<-------------------  Video Title  ---------------------->\n")
-    #get Video Title
-    print(video_object.title)
+    return(video_object)
 
-    print("\n<--------------  Display available streams  -------------->")
-    print("""\nThese streams are video and audio in one. 
-            For video only or audio only, modify stream filter in sourcecode\n""")
-   
+def get_all_video_streams(video_object):
+    print("\n<--------------  Display audio+video streams  -------------->")
     ###>>> get all the streams with video+audio 
     for stream in video_object.streams.filter(progressive=True):
         print(stream)
-
+    
     print("\n<--------------  Display audio-only streams  -------------->")
     print("""\nThese streams are audio only ie only_audio=True.\n""")
 
     ###>>> get all the streams with video+audio
     for stream in video_object.streams.filter(only_audio=True):
         print(stream)
+    
 
+
+def get_video_info(video_object):
+    print('\n<------------------------  Video info  --------------------------------->\n')
+    print(f'title:  {video_object.title}')
+    print(f'length: {round(video_object.length / 60,2)} minutes')
+    print(f'views:  {round(video_object.views / 1000000,3)} million')
+    print(f'author: {video_object.author}')
+    print('\n<---------------------------------------------------------------------->\n')
+
+
+def dl_video(video_object):
     print("\n<---------------  Stream selection  ---------------------->\n")
     # select preferred stream
     chosen_stream = int(input("Which itag do you choose?: "))
@@ -53,7 +53,8 @@ def dl_video(url):
     print("\n<----------------  Download finished  -------------------->\n")
 
 if __name__ == "__main__":
-    
     url = str(input("Give video url: "))
-    get_video_info(url)
-    dl_video(url)
+    video_object = get_video_object(url)
+    get_video_info(video_object)
+    get_all_video_streams(video_object)
+    dl_video(video_object)
